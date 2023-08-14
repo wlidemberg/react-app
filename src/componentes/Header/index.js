cb130258ed35c699bc73da79724bc3f3ffc1d11a
Berg Sousa <automatizei@gmail.com>
2023-08-14 17:27:12 -0300

    menu mobile com resolução ate 1024px

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { CgMenuRound } from 'react-icons/cg';
 import './style.css';
 
 export default function Header(){
+    const [menuResponse, setMenuResponse] = useState(false);
+    const toggleMenu = () => {
+        setMenuResponse(!menuResponse);
+    }
+
+    const handleLinkClick = () => {
+        setMenuResponse(false);
+    }
     return(
         <header>
             <div className='container topo'>
@@ -9,13 +18,14 @@ export default function Header(){
                     <img src='/assets/rwmaster_logo.png' alt='logo RW Master Contabilidade'/>
                 </div>
                 <nav className='navegacao'>
-                    <ul className='navegacao-menu'>
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/servicos'>Serviços</a></li>
-                        <li><a href='/produtos'>Produtos</a></li>
-                        <li><a href='/contato'>Contato</a></li>
-                        <li><a href='/area-cliente' className='btn-area-cliente'><span><img src='/assets/enter.png' alt='icon enter'/></span>Area do Cliente</a></li>
+                    <ul className={`navegacao-menu ${menuResponse ? 'navegacao-menu-open' : ''}`}>
+                        <li><a href='/' onClick={handleLinkClick}>Home</a></li>
+                        <li><a href='/servicos' onClick={handleLinkClick}>Serviços</a></li>
+                        <li><a href='/produtos' onClick={handleLinkClick}>Produtos</a></li>
+                        <li><a href='/contato' onClick={handleLinkClick}>Contato</a></li>
+                        <li><a href='/area-cliente' className='btn-area-cliente'><span><img src='/assets/enter.png' alt='icon enter' onClick={handleLinkClick}/></span>Area do Cliente</a></li>
                     </ul>
+                    <button className='btn-response' onClick={toggleMenu}><CgMenuRound className='btn-res'/></button>   
                 </nav>
             </div>
         </header>
